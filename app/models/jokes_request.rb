75790f5368bd63a2deaa53428dd1d5896dd88376
maikhel <mlecicki@gmail.com>
2024-02-14 15:36:02 +0100

    Small adjustments: use new symbol syntax in model, update styles

--- app/models/jokes_request.rb
+++ app/models/jokes_request.rb
@@ -1,5 +1,5 @@
 class JokesRequest < ApplicationRecord
   has_many :jokes, dependent: :destroy
 
-  validates :amount, :numericality => { greater_than: 0, less_than: 100 }
+  validates :amount, numericality: { greater_than: 0, less_than: 100 }
 end
