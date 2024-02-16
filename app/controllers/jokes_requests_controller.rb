a374167c3016c9ede04c70e8d0f39152a19f0e4c
maikhel <mlecicki@gmail.com>
2024-02-16 20:26:48 +0100

    Render new request form on the index page

--- app/controllers/jokes_requests_controller.rb
+++ app/controllers/jokes_requests_controller.rb
@@ -2,7 +2,7 @@ class JokesRequestsController < ApplicationController
   before_action :set_jokes_request, only: %i[ show edit update destroy ]
 
   def index
-    @jokes_requests = JokesRequest.all.order(id: :desc)
+    @jokes_requests = JokesRequest.all.order(id: :asc)
   end
 
   def show
