014c60e88d0dfc6252eb4ab8d628eeb4339350f6
maikhel <mlecicki@gmail.com>
2024-02-16 15:04:57 +0100

    Add turbo stream for jokes count

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -4,4 +4,6 @@ class Joke < ApplicationRecord
   validates :body, presence: true
 
   broadcasts_to ->(joke) { [ joke.jokes_request, "jokes" ] }, inserts_by: :append
+
+  after_create_commit ->(joke) { broadcast_replace_to([ joke.jokes_request, "jokes_count" ], target: "jokes_count", partial: "jokes_requests/jokes_count", locals: { jokes_request: joke.jokes_request }) }
 end
